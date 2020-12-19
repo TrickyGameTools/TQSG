0a206e73235449c978214ecd1f79c9be4ba82e4c
Jeroen Broks <facebook@tbbs.nl>
2020-12-19 02:49:56 +0100

    I did it... I wonder what I did

--- TQSA.cpp
+++ TQSA.cpp
@@ -1,7 +1,7 @@
 // Lic:
 // TQSA.cpp
 // Tricky's Quick SDL Audio
-// version: 20.09.05
+// version: 20.12.19
 // Copyright (C) 2020 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
